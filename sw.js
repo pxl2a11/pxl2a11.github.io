910e0baa4acdf136483468d4e0dd06cd2e0ce4b6
pxl2a11 <pxl2a11@gmail.com>
2025-09-08 14:41:34 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // sw.js
 
-const CACHE_NAME = 'mini-apps-cache-v34'; // ВЕРСИЯ КЭША ОБНОВЛЕНА!
+const CACHE_NAME = 'mini-apps-cache-v35'; // ВЕРСИЯ КЭША ОБНОВЛЕНА!
 const APP_SHELL_URL = '/index.html';
 const OFFLINE_URL = '/offline.html';
 
@@ -18,7 +18,8 @@ const appModules = [
     'flappyBird',
     'changelogPage',
     'textAnalyzer',
-    'virtualDice'
+    'virtualDice',
+    'calorieCalculator'
 ];
 
 // Автоматически генерируем пути к файлам на основе списка модулей
