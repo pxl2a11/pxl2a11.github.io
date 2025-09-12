bef4ca73d9bd7835a645d3abef77acfb29059874
pxl2a11 <pxl2a11@gmail.com>
2025-09-12 10:59:33 +0300

    Update radioStationsData.js

--- js/radioStationsData.js
+++ js/radioStationsData.js
@@ -32,7 +32,11 @@ export const radioStations = [
         { name: "192 kbps", url: "https://radio-srv1.11one.ru/record192k.mp3" }] },
     { name: "Маруся FM", logoUrl: "https://pcradio.ru/images/stations/61b09cf176d9e.jpg", streams: [{ name: "Основной поток", url: "https://msk8.radio-holding.ru/marusya_default" }] },
     { name: "Record Russian Hits", logoUrl: "https://pcradio.ru/images/stations/61b09ca2ead8f.jpg", streams: [{ name: "Основной поток", url: "https://stream.pcradio.ru/record_rushits-med" }] },
-    { name: "Новое Радио", logoUrl: "https://pcradio.ru/images/stations/62ea3eb91b608.jpg", streams: [{ name: "Основной поток", url: "https://stream.pcradio.ru/novoe_radio98_4-hi" }] },
+    { name: "Новое Радио", logoUrl: "https://pcradio.ru/images/stations/62ea3eb91b608.jpg", streams: [{ name: "192 kbps", url: "https://stream.newradio.ru/novoe96.aacp" }] },
+    { name: "Новое Радио", logoUrl: "https://pcradio.ru/images/stations/62ea3eb91b608.jpg", streams: [
+        { name: "64 kbps", url: "https://emgspb.hostingradio.ru/novoespb64.mp3" },
+        { name: "96 kbps", url: "https://stream.newradio.ru/novoe96.aacp" },
+        { name: "128 kbps", url: "https://emgspb.hostingradio.ru/novoespb128.mp3" }] },
     { name: "Дорожное Радио", logoUrl: "https://top-radio.ru/assets/image/radio/180/dorojnoe.png", streams: [
         { name: "32 kbps", url: "https://hls-01-dorognoe.emgsound.ru/15/32/playlist.m3u8" },
         { name: "64 kbps", url: "https://hls-01-dorognoe.emgsound.ru/15/64/playlist.m3u8" },
