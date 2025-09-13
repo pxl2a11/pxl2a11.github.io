9e21e26c2b0ce032c26a5f35c2e0754c4bc6d3da
pxl2a11 <pxl2a11@gmail.com>
2025-09-13 21:37:47 +0300

    Update tvChannelsData.js

--- js/tvChannelsData.js
+++ js/tvChannelsData.js
@@ -2,16 +2,16 @@
 
 export const tvChannels = [
     {
-        name: "Первый канал",
+        name: "Первый канал HD",
         logoUrl: "https://smotret.tv/images/1-kanal.webp",
         type: "hls", // Указываем тип потока
-        streamUrl: "https://rt-vlg-nn-htlive.cdn.ngenix.net/hls/CH_R05_KAMINTV/variant.m3u8"
+        streamUrl: "https://edge1.1internet.tv/dash-live2/streams/1tv-dvr/1tvdash.mpd"
     },
     {
-        name: "Первый канал HD",
+        name: "Первый канал FHD",
         logoUrl: "https://smotret.tv/images/1-kanal-hd.webp",
         type: "hls", // Указываем тип потока
-        streamUrl: "https://live-mirror-01.ott.tricolor.tv/live/live/1TV_hd/hls_enc/1TV_hd.m3u8?drmreq=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBfdHlwZSI6ImFwdHBfb3R0IiwiY2xhc3MiOiJCUk9XU0VSIiwiZHJlaWQiOiI0NTAyNTUwMDM1ODM5NSIsImV4cCI6MTc1ODA3NDQyNywiaHdpZCI6IjhkMjA0NWE1N2E3NjVkYzUyMjVmYzJkZmRlNWY0NzViMDA0NzU0MmFlZDhjMWEyNGNlY2YzODNkY2ZjYmY2MjYiLCJsYXN0X3JlcV9kYXRlIjoxNzU3NDY5NjI3LCJzZXNzaW9uX2lkIjoiZDlmZTgxOTFmYTFjMjQ4ZjUzN2ViNjVlOWE0M2ZjOGQiLCJ0b2tlbl90eXBlIjoiQ29udGVudEFjY2Vzc1Rva2VuIiwidHlwZSI6Ik1PWklMTEEifQ.g1L-cwTkjltXHyAVVKPyFgLG7fmTOOKtj33WrsSL8hE"
+        streamUrl: "http://edge4.1internet.tv/dash-live2/streams/1tv-dvr/1tvdash.mpd"
     },
        {
         name: "Россия 1",
