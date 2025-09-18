62beaf04060926fedd52e546d80bde49dd6b9bb9
pxl2a11 <pxl2a11@gmail.com>
2025-09-18 14:40:25 +0300

    Update tvChannelsData.js

--- js/tvChannelsData.js
+++ js/tvChannelsData.js
@@ -3,33 +3,7 @@
 export const tvChannels = [
 
     { 
-        name: "ТВ Центр", 
-        logoUrl: "https://filearchive.cnews.ru/img/book/2022/01/19/tvcentr.jpg",
-        embedHtml: `
-            <iframe
-                width="100%"
-                height="100%"
-                src="https://vkvideo.ru/video_ext.php?oid=-98976670&id=456248870&hd=2&autoplay=1"
-                style="border: none; border-radius: 0.5rem;"
-                allow="clipboard-write; autoplay"
-                allowFullScreen
-            ></iframe>`
-    },
-    { 
-        name: "СТС", 
-        logoUrl: "https://typetype.ru/wp-content/uploads/font-in-use_sts_1.png",
-        embedHtml: `
-            <iframe
-                width="100%"
-                height="100%"
-                src="https://vkvideo.ru/video_ext.php?oid=-18496184&id=456260645&hd=2&autoplay=1"
-                style="border: none; border-radius: 0.5rem;"
-                allow="clipboard-write; autoplay"
-                allowFullScreen
-            ></iframe>`
-    },
-    { 
-        name: "ОТР", 
+        name: "Общественное телевидение России", 
         logoUrl: "https://onlitvtime.ru/media/AEbLJVF7qXeWEKpEf7pQyWKjdI4jbcfPiaAxmoVm.png",
         embedHtml: `
             <iframe
@@ -42,117 +16,13 @@ export const tvChannels = [
             ></iframe>`
     },
     { 
-        name: "Домашний", 
-        logoUrl: "https://www.oreol.tv/img/014_domash_1.jpg",
+        name: "ТВ Центр", 
+        logoUrl: "https://filearchive.cnews.ru/img/book/2022/01/19/tvcentr.jpg",
         embedHtml: `
             <iframe
                 width="100%"
                 height="100%"
-                src="https://vkvideo.ru/video_ext.php?oid=-30584503&id=456259900&hd=2&autoplay=1"
-                style="border: none; border-radius: 0.5rem;"
-                allow="clipboard-write; autoplay"
-                allowFullScreen
-            ></iframe>`
-    },
-    { 
-        name: "МУЗ-ТВ", 
-        logoUrl: "https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/022023/muz_tv_0.png",
-        embedHtml: `
-            <iframe
-                width="100%"
-                height="100%"
-                src="https://vkvideo.ru/video_ext.php?oid=-6246566&id=456246663&hd=2&autoplay=1"
-                style="border: none; border-radius: 0.5rem;"
-                allow="clipboard-write; autoplay"
-                allowFullScreen
-            ></iframe>`
-    },
-    { 
-        name: "Звезда", 
-        logoUrl: "https://ministar.ru/upload/iblock/dd9/t9388lfeudisqk09hzyonadct1hinn1t.png",
-        embedHtml: `
-            <iframe
-                width="100%"
-                height="100%"
-                src="https://vkvideo.ru/video_ext.php?oid=-220155889&id=456254118&hd=2&autoplay=1"
-                style="border: none; border-radius: 0.5rem;"
-                allow="clipboard-write; autoplay"
-                allowFullScreen
-            ></iframe>`
-    },
-    { 
-        name: "Че!", 
-        logoUrl: "https://www.tvreclama.ru/upload/iblock/c57/c574253a41104fe044dbd4ee8bb0b34d.jpg",
-        embedHtml: `
-            <iframe
-                width="100%"
-                height="100%"
-                src="https://vkvideo.ru/video_ext.php?oid=-19555151&id=456245834&hd=2&autoplay=1"
-                style="border: none; border-radius: 0.5rem;"
-                allow="clipboard-write; autoplay"
-                allowFullScreen
-            ></iframe>`
-    },
-    { 
-        name: "Ю", 
-        logoUrl: "https://рекламанащитах.рф/wp-content/uploads/2021/01/%D1%8E-1024x1024.jpg",
-        embedHtml: `
-            <iframe
-                width="100%"
-                height="100%"
-                src="https://vkvideo.ru/video_ext.php?oid=-41903770&id=456248205&hd=2&autoplay=1"
-                style="border: none; border-radius: 0.5rem;"
-                allow="clipboard-write; autoplay"
-                allowFullScreen
-            ></iframe>`
-    },
-    { 
-        name: "2х2", 
-        logoUrl: "https://papik.pro/grafic/uploads/posts/2023-04/thumbs/1681574310_papik-pro-p-2kh2-logotip-vektor-14.jpg",
-        embedHtml: `
-            <iframe
-                width="100%"
-                height="100%"
-                src="https://vkvideo.ru/video_ext.php?oid=-48864&id=456249172&hd=2&autoplay=1"
-                style="border: none; border-radius: 0.5rem;"
-                allow="clipboard-write; autoplay"
-                allowFullScreen
-            ></iframe>`
-    },
-    { 
-        name: "СТС Love", 
-        logoUrl: "https://papik.pro/grafic/uploads/posts/2023-04/1681574795_papik-pro-p-sts-lav-logotip-vektor-2.png",
-        embedHtml: `
-            <iframe
-                width="100%"
-                height="100%"
-                src="https://vkvideo.ru/video_ext.php?oid=-68488417&id=456242742&hd=2&autoplay=1"
-                style="border: none; border-radius: 0.5rem;"
-                allow="clipboard-write; autoplay"
-                allowFullScreen
-            ></iframe>`
-    },
-    { 
-        name: "Суббота!", 
-        logoUrl: "https://vits.tv/images/tvstation/russia/subbota.png",
-        embedHtml: `
-            <iframe
-                width="100%"
-                height="100%"
-                src="https://vkvideo.ru/video_ext.php?oid=-155566339&id=456243314&hd=2&autoplay=1"
-                style="border: none; border-radius: 0.5rem;"
-                allow="clipboard-write; autoplay"
-                allowFullScreen
-            ></iframe>`
-    },
-    { 
-        name: "Солнце", 
-        logoUrl: "https://sun-tv.ru/static/images/img/about/logo.png",
-        embedHtml: `
-            <iframe
-                width="100%"
-                height="100%"
-                src="https://vkvideo.ru/video_ext.php?oid=-217596589&id=456240257&hd=2&autoplay=1"
+                src="https://vkvideo.ru/video_ext.php?oid=-98976670&id=456248870&hd=2&autoplay=1"
                 style="border: none; border-radius: 0.5rem;"
                 allow="clipboard-write; autoplay"
                 allowFullScreen
@@ -172,13 +42,130 @@ export const tvChannels = [
             ></iframe>`
     },
     { 
-        name: "МИР 24", 
-        logoUrl: "https://paralymp.ru/upload/iblock/c39/c39da8b1ed6a31be20a2ca151b0219c0.jpg",
+        name: "СТС", 
+        logoUrl: "https://typetype.ru/wp-content/uploads/font-in-use_sts_1.png",
         embedHtml: `
             <iframe
                 width="100%"
                 height="100%"
-                src="https://vkvideo.ru/video_ext.php?oid=-23712441&id=456270471&hd=2&autoplay=1"
+                src="https://vkvideo.ru/video_ext.php?oid=-18496184&id=456260645&hd=2&autoplay=1"
+                style="border: none; border-radius: 0.5rem;"
+                allow="clipboard-write; autoplay"
+                allowFullScreen
+            ></iframe>`
+    },
+    { 
+        name: "Домашний", 
+        logoUrl: "https://www.oreol.tv/img/014_domash_1.jpg",
+        embedHtml: `
+            <iframe
+                width="100%"
+                height="100%"
+                src="https://vkvideo.ru/video_ext.php?oid=-30584503&id=456259900&hd=2&autoplay=1"
+                style="border: none; border-radius: 0.5rem;"
+                allow="clipboard-write; autoplay"
+                allowFullScreen
+            ></iframe>`
+    },
+    { 
+        name: "Звезда", 
+        logoUrl: "https://ministar.ru/upload/iblock/dd9/t9388lfeudisqk09hzyonadct1hinn1t.png",
+        embedHtml: `
+            <iframe
+                width="100%"
+                height="100%"
+                src="https://vkvideo.ru/video_ext.php?oid=-220155889&id=456254118&hd=2&autoplay=1"
+                style="border: none; border-radius: 0.5rem;"
+                allow="clipboard-write; autoplay"
+                allowFullScreen
+            ></iframe>`
+    },
+    { 
+        name: "МУЗ-ТВ", 
+        logoUrl: "https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/022023/muz_tv_0.png",
+        embedHtml: `
+            <iframe
+                width="100%"
+                height="100%"
+                src="https://vkvideo.ru/video_ext.php?oid=-6246566&id=456246663&hd=2&autoplay=1"
+                style="border: none; border-radius: 0.5rem;"
+                allow="clipboard-write; autoplay"
+                allowFullScreen
+            ></iframe>`
+    },
+    { 
+        name: "78", 
+        logoUrl: "https://www.sostav.ru/app/public/images/news/2017/08/03/compressed/78%20(3).jpg",
+        embedHtml: `
+            <iframe
+                width="100%"
+                height="100%"
+                src="https://vkvideo.ru/video_ext.php?oid=-148235461&id=456239063&hd=2&autoplay=1"
+                style="border: none; border-radius: 0.5rem;"
+                allow="clipboard-write; autoplay"
+                allowFullScreen
+            ></iframe>`
+    },
+    { 
+        name: "Че!", 
+        logoUrl: "https://www.tvreclama.ru/upload/iblock/c57/c574253a41104fe044dbd4ee8bb0b34d.jpg",
+        embedHtml: `
+            <iframe
+                width="100%"
+                height="100%"
+                src="https://vkvideo.ru/video_ext.php?oid=-19555151&id=456245834&hd=2&autoplay=1"
+                style="border: none; border-radius: 0.5rem;"
+                allow="clipboard-write; autoplay"
+                allowFullScreen
+            ></iframe>`
+    },
+    { 
+        name: "Суббота!", 
+        logoUrl: "https://vits.tv/images/tvstation/russia/subbota.png",
+        embedHtml: `
+            <iframe
+                width="100%"
+                height="100%"
+                src="https://vkvideo.ru/video_ext.php?oid=-155566339&id=456243314&hd=2&autoplay=1"
+                style="border: none; border-radius: 0.5rem;"
+                allow="clipboard-write; autoplay"
+                allowFullScreen
+            ></iframe>`
+    },
+    { 
+        name: "СТС Love", 
+        logoUrl: "https://papik.pro/grafic/uploads/posts/2023-04/1681574795_papik-pro-p-sts-lav-logotip-vektor-2.png",
+        embedHtml: `
+            <iframe
+                width="100%"
+                height="100%"
+                src="https://vkvideo.ru/video_ext.php?oid=-68488417&id=456242742&hd=2&autoplay=1"
+                style="border: none; border-radius: 0.5rem;"
+                allow="clipboard-write; autoplay"
+                allowFullScreen
+            ></iframe>`
+    },
+    { 
+        name: "2х2", 
+        logoUrl: "https://papik.pro/grafic/uploads/posts/2023-04/thumbs/1681574310_papik-pro-p-2kh2-logotip-vektor-14.jpg",
+        embedHtml: `
+            <iframe
+                width="100%"
+                height="100%"
+                src="https://vkvideo.ru/video_ext.php?oid=-48864&id=456249172&hd=2&autoplay=1"
+                style="border: none; border-radius: 0.5rem;"
+                allow="clipboard-write; autoplay"
+                allowFullScreen
+            ></iframe>`
+    },
+    { 
+        name: "Солнце", 
+        logoUrl: "https://sun-tv.ru/static/images/img/about/logo.png",
+        embedHtml: `
+            <iframe
+                width="100%"
+                height="100%"
+                src="https://vkvideo.ru/video_ext.php?oid=-217596589&id=456240257&hd=2&autoplay=1"
                 style="border: none; border-radius: 0.5rem;"
                 allow="clipboard-write; autoplay"
                 allowFullScreen
@@ -198,13 +185,13 @@ export const tvChannels = [
             ></iframe>`
     },
     { 
-        name: "World Fashion Channel", 
-        logoUrl: "https://i.pinimg.com/280x280_RS/6a/17/84/6a1784fcb7ddbb39bdc8ef9577c95d88.jpg",
+        name: "Ю", 
+        logoUrl: "https://рекламанащитах.рф/wp-content/uploads/2021/01/%D1%8E-1024x1024.jpg",
         embedHtml: `
             <iframe
                 width="100%"
                 height="100%"
-                src="https://vkvideo.ru/video_ext.php?oid=-41395497&id=456240883&hd=2&autoplay=1"
+                src="https://vkvideo.ru/video_ext.php?oid=-41903770&id=456248205&hd=2&autoplay=1"
                 style="border: none; border-radius: 0.5rem;"
                 allow="clipboard-write; autoplay"
                 allowFullScreen
@@ -223,14 +210,27 @@ export const tvChannels = [
                 allowFullScreen
             ></iframe>`
     },
-        { 
-        name: "78", 
-        logoUrl: "https://www.sostav.ru/app/public/images/news/2017/08/03/compressed/78%20(3).jpg",
+    { 
+        name: "World Fashion Channel", 
+        logoUrl: "https://i.pinimg.com/280x280_RS/6a/17/84/6a1784fcb7ddbb39bdc8ef9577c95d88.jpg",
         embedHtml: `
             <iframe
                 width="100%"
                 height="100%"
-                src="https://vkvideo.ru/video_ext.php?oid=-148235461&id=456239063&hd=2&autoplay=1"
+                src="https://vkvideo.ru/video_ext.php?oid=-41395497&id=456240883&hd=2&autoplay=1"
+                style="border: none; border-radius: 0.5rem;"
+                allow="clipboard-write; autoplay"
+                allowFullScreen
+            ></iframe>`
+    },
+    { 
+        name: "МИР 24", 
+        logoUrl: "https://paralymp.ru/upload/iblock/c39/c39da8b1ed6a31be20a2ca151b0219c0.jpg",
+        embedHtml: `
+            <iframe
+                width="100%"
+                height="100%"
+                src="https://vkvideo.ru/video_ext.php?oid=-23712441&id=456270471&hd=2&autoplay=1"
                 style="border: none; border-radius: 0.5rem;"
                 allow="clipboard-write; autoplay"
                 allowFullScreen
