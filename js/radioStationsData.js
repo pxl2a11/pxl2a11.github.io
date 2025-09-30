ef17c1681e013b958215a7a3c9106e53ee9f89ae
pxl2a11 <pxl2a11@gmail.com>
2025-09-30 13:15:00 +0300

    Update radioStationsData.js

--- js/radioStationsData.js
+++ js/radioStationsData.js
@@ -87,13 +87,13 @@ export const radioStations = [
         { name: "128 kbps", url: "https://air2.volna.top/Vanya-SPB" }] },
     { name: "Радио Эрмитаж", logoUrl: "img/radio/radio_ermitazh.png", streams: [
         { name: "128 kbps", url: "https://hermitage.hostingradio.ru/hermitage128.mp3" }] },
-    { name: "Радио Зенит", logoUrl: "https://top-radio.ru/assets/image/radio/180/zenit-fm.jpg", streams: [
+    { name: "Радио Зенит", logoUrl: "img/radio/radio_zenit.png", streams: [
         { name: "256 kbps", url: "https://radiozenit.hostingradio.ru/radiozenit256.mp3" }] },
     { name: "Вести ФМ", logoUrl: "img/radio/vesti_fm.png", streams: [
         { name: "64 kbps", url: "https://icecast-vgtrk.cdnvideo.ru/vestifm_mp3_64kbps" },
         { name: "128 kbps", url: "https://icecast-vgtrk.cdnvideo.ru/vestifm_mp3_128kbps" },
         { name: "192 kbps", url: "https://icecast-vgtrk.cdnvideo.ru/vestifm_mp3_192kbps" }] },
-    { name: "Юмор FM", logoUrl: "img/radio/yumor fm.png", streams: [
+    { name: "Юмор FM", logoUrl: "img/radio/yumor_fm.png", streams: [
         { name: "32 kbps", url: "https://hls-01-gpm.hostingradio.ru/humorfm495/32/playlist.m3u8" },
         { name: "64 kbps", url: "https://hls-01-gpm.hostingradio.ru/humorfm495/64/playlist.m3u8" },
         { name: "128 kbps", url: "https://hls-01-gpm.hostingradio.ru/humorfm495/128/playlist.m3u8" }] },
@@ -109,42 +109,73 @@ export const radioStations = [
         { name: "32 kbps", url: "https://hls-01-dorognoe.emgsound.ru/15_spb/32/playlist.m3u8" },
         { name: "64 kbps", url: "https://hls-01-dorognoe.emgsound.ru/15_spb/64/playlist.m3u8" },
         { name: "112 kbps", url: "https://hls-01-regions.emgsound.ru/15_spb/112/playlist.m3u8" }] },
-    { name: "Маруся FM", logoUrl: "https://pcradio.ru/images/stations/61b09cf176d9e.jpg", streams: [{ name: "Основной поток", url: "https://msk8.radio-holding.ru/marusya_default" }] },
-    { name: "Record Russian Hits", logoUrl: "https://pcradio.ru/images/stations/61b09ca2ead8f.jpg", streams: [{ name: "Основной поток", url: "https://stream.pcradio.ru/record_rushits-med" }] },
-    { name: "Relax FM", logoUrl: "https://pcradio.ru/images/stations/62ea3ebda8c01.jpg", streams: [
+    { name: "Маруся FM", logoUrl: "img/radio/marusya_fm.png", streams: [{ name: "Основной поток", url: "https://msk8.radio-holding.ru/marusya_default" }] },
+    { name: "Record Russian Hits", logoUrl: "img/radio/record_russian_hits.png", streams: [{ name: "Основной поток", url: "https://stream.pcradio.ru/record_rushits-med" }] },
+    { name: "Relax FM", logoUrl: "img/radio/relax_fm.png", streams: [
         { name: "64 kbps", url: "https://pub0201.101.ru/stream/air/aac/64/200" },
         { name: "128 kbps", url: "https://hls-01-gpm.hostingradio.ru/relaxfm495/128/playlist.m3u8" }] },
-    { name: "Соль FM", logoUrl: "https://pcradio.ru/images/stations/61b09cf2dc6d7.jpg", streams: [{ name: "Основной поток", url: "https://stream.pcradio.ru/sol_fm-hi" }] },
-    { name: "РУССКАЯ ВОЛНА", logoUrl: "https://amgradio.ru/img/RuWavebg.svg", streams: [{ name: "Поток 48kbps", url: "https://ru1.amgradio.ru/RuWave48" }] },
-    { name: "РЕТРО ХИТ", logoUrl: "https://amgradio.ru/img/retrohit.svg", streams: [{ name: "Основной поток", url: "https://retro.amgradio.ru/Retro" }] },
-    { name: "РУССКИЙ РОК", logoUrl: "https://amgradio.ru/img/rusrock.svg", streams: [{ name: "Основной поток", url: "https://rock.amgradio.ru/RusRock" }] },
-    { name: "ХАЙП FM", logoUrl: "https://amgradio.ru/img/hypefm.svg", streams: [{ name: "Основной поток", url: "https://hfm.amgradio.ru/HypeFM" }] },
-    { name: "REMIX FM", logoUrl: "https://amgradio.ru/img/remix.svg", streams: [{ name: "Основной поток", url: "https://rmx.amgradio.ru/RemixFM" }] },
-    { name: "DEEP FM", logoUrl: "https://amgradio.ru/img/deepfm.svg", streams: [{ name: "Основной поток", url: "https://deep.amgradio.ru/DeepFM" }] },
-    { name: "MEGA RADIO", logoUrl: "https://amgradio.ru/img/megaradio.svg", streams: [{ name: "Основной поток", url: "https://mega.amgradio.ru/mega" }] },
-    { name: "RADIO CAFE", logoUrl: "https://amgradio.ru/img/radiocafe.svg", streams: [{ name: "Основной поток", url: "https://cafe.amgradio.ru/Cafe" }] },
-    { name: "ХОРОШЕЕ РАДИО", logoUrl: "https://amgradio.ru/img/horoshee.svg", streams: [{ name: "Основной поток", url: "https://hr.amgradio.ru/Horoshee" }] },
-    { name: "РУССКОЕ FM", logoUrl: "https://amgradio.ru/img/rufm.svg", streams: [{ name: "Основной поток", url: "https://rufm.amgradio.ru/rufm" }] },
-    { name: "RADIO FRESH", logoUrl: "https://amgradio.ru/img/fresh.svg", streams: [{ name: "Основной поток", url: "https://fresh.amgradio.ru/Fresh" }] },
-    { name: "МУРКА FM", logoUrl: "https://amgradio.ru/img/murkafm.svg", streams: [{ name: "Основной поток", url: "https://murka.amgradio.ru/MurkaFM" }] },
-    { name: "РОДНЫЕ ПЕСНИ", logoUrl: "https://amgradio.ru/img/rodnyepesni.svg", streams: [{ name: "Основной поток", url: "https://rodpesni.amgradio.ru/rp" }] },
-    { name: "РУССКИЕ ПЕСНИ", logoUrl: "https://amgradio.ru/img/rusongswhite.svg", streams: [{ name: "Основной поток", url: "https://rusongs.amgradio.ru/rusongs" }] },
-    { name: "DJIN FM", logoUrl: "https://amgradio.ru/img/djin.svg", streams: [{ name: "Основной поток", url: "https://djin.amgradio.ru/djin" }] },
-    { name: "ШТОРМ FM", logoUrl: "https://amgradio.ru/img/shtormfm.svg", streams: [{ name: "Основной поток", url: "https://shtorm.amgradio.ru/shtormfm" }] },
-    { name: "MUSIC BOX RADIO", logoUrl: "https://amgradio.ru/img/musicboxradio.svg", streams: [{ name: "Основной поток", url: "https://musicbox.amgradio.ru/musicbox" }] },
-    { name: "CHILLA FM", logoUrl: "https://amgradio.ru/img/chillafm.svg", streams: [{ name: "Основной поток", url: "https://chilla.amgradio.ru/ChillaFM" }] },
-    { name: "LITE FM", logoUrl: "https://amgradio.ru/img/litefm.svg", streams: [{ name: "Основной поток", url: "https://litefm.amgradio.ru/litefm" }] },
-    { name: "ДЕТСКИЙ ХИТ", logoUrl: "https://amgradio.ru/img/detifm.svg", streams: [{ name: "Основной поток", url: "https://deti.amgradio.ru/Deti" }] },
-    { name: "СКАЗКА FM", logoUrl: "https://amgradio.ru/img/skazkafm.svg", streams: [{ name: "Основной поток", url: "https://skazka.amgradio.ru/Skazka" }] },
-    { name: "УМНОЕ РАДИО", logoUrl: "https://amgradio.ru/img/umnoe.svg", streams: [{ name: "Основной поток", url: "https://umnoe.amgradio.ru/Umnoe" }] },
-    { name: "LATINO RADIO", logoUrl: "https://amgradio.ru/img/latino.svg", streams: [{ name: "Основной поток", url: "https://latino.amgradio.ru/latino" }] },
-    { name: "РАДИО МЕЧТА", logoUrl: "https://amgradio.ru/img/mechta.svg", streams: [{ name: "Основной поток", url: "https://mechta.amgradio.ru/mechta" }] },
-    { name: "RADIO FUNK", logoUrl: "https://amgradio.ru/img/radiofunk.svg", streams: [{ name: "Основной поток", url: "https://funk.amgradio.ru/Funk" }] },
-    { name: "RBS FM", logoUrl: "https://amgradio.ru/img/rbsfm.svg", streams: [{ name: "Основной поток", url: "https://rbs.amgradio.ru/rbs" }] },
-    { name: "MAKKIRUS", logoUrl: "https://amgradio.ru/img/makkirus.svg", streams: [{ name: "Основной поток", url: "https://makkirus.amgradio.ru/makkirus" }] },
-    { name: "CLASSIC FM", logoUrl: "https://amgradio.ru/img/classicfm.svg", streams: [{ name: "Основной поток", url: "https://classic.amgradio.ru/ClassicFM" }] },
-    { name: "РАДИО ЗВУКИ ПРИРОДЫ", logoUrl: "https://amgradio.ru/img/zvuki.svg", streams: [{ name: "Основной поток", url: "https://zvuki.amgradio.ru/Zvuki" }] },
-    { name: "Спутник Радио", logoUrl: "https://pcradio.ru/images/stations/62ea3ebe43be0.jpg", streams: [{ name: "Основной поток", url: "https://stream.pcradio.ru/sputnik_ria_ru-med" }] },
-    { name: "ТНТ Music Radio", logoUrl: "https://pcradio.ru/images/stations/61b09c403d308.jpg", streams: [
+    { name: "Соль FM", logoUrl: "img/radio/sol_fm.png", streams: [
+        { name: "Основной поток", url: "https://stream.pcradio.ru/sol_fm-hi" }] },
+    { name: "РУССКАЯ ВОЛНА", logoUrl: "img/radio/russkaya_volna.png", streams: [
+        { name: "Поток 48kbps", url: "https://ru1.amgradio.ru/RuWave48" }] },
+    { name: "РЕТРО ХИТ", logoUrl: "img/radio/retro_hit.png", streams: [
+        { name: "Основной поток", url: "https://retro.amgradio.ru/Retro" }] },
+    { name: "РУССКИЙ РОК", logoUrl: "img/radio/russkij_rok.png", streams: [
+        { name: "Основной поток", url: "https://rock.amgradio.ru/RusRock" }] },
+    { name: "ХАЙП FM", logoUrl: "img/radio/hajp_fm.png", streams: [
+        { name: "Основной поток", url: "https://hfm.amgradio.ru/HypeFM" }] },
+    { name: "REMIX FM", logoUrl: "img/radio/remix_fm.png", streams: [
+        { name: "Основной поток", url: "https://rmx.amgradio.ru/RemixFM" }] },
+    { name: "DEEP FM", logoUrl: "img/radio/deep_fm.png", streams: [
+        { name: "Основной поток", url: "https://deep.amgradio.ru/DeepFM" }] },
+    { name: "MEGA RADIO", logoUrl: "img/radio/mega_radio.png", streams: [
+        { name: "Основной поток", url: "https://mega.amgradio.ru/mega" }] },
+    { name: "RADIO CAFE", logoUrl: "img/radio/radio_cafe.png", streams: [
+        { name: "Основной поток", url: "https://cafe.amgradio.ru/Cafe" }] },
+    { name: "ХОРОШЕЕ РАДИО", logoUrl: "img/radio/horoshee_radio.png", streams: [
+        { name: "Основной поток", url: "https://hr.amgradio.ru/Horoshee" }] },
+    { name: "РУССКОЕ FM", logoUrl: "img/radio/russkoe_fm.png", streams: [
+        { name: "Основной поток", url: "https://rufm.amgradio.ru/rufm" }] },
+    { name: "RADIO FRESH", logoUrl: "img/radio/radio_fresh.png", streams: [
+        { name: "Основной поток", url: "https://fresh.amgradio.ru/Fresh" }] },
+    { name: "МУРКА FM", logoUrl: "img/radio/murka_fm.png", streams: [
+        { name: "Основной поток", url: "https://murka.amgradio.ru/MurkaFM" }] },
+    { name: "РОДНЫЕ ПЕСНИ", logoUrl: "img/radio/rodnye_pesni.png", streams: [
+        { name: "Основной поток", url: "https://rodpesni.amgradio.ru/rp" }] },
+    { name: "РУССКИЕ ПЕСНИ", logoUrl: "img/radio/russkie_pesni.png", streams: [
+        { name: "Основной поток", url: "https://rusongs.amgradio.ru/rusongs" }] },
+    { name: "DJIN FM", logoUrl: "img/radio/djin_fm.png", streams: [
+        { name: "Основной поток", url: "https://djin.amgradio.ru/djin" }] },
+    { name: "ШТОРМ FM", logoUrl: "img/radio/shtorm_fm.png", streams: [
+        { name: "Основной поток", url: "https://shtorm.amgradio.ru/shtormfm" }] },
+    { name: "MUSIC BOX RADIO", logoUrl: "img/radio/music_box_radio.png", streams: [
+        { name: "Основной поток", url: "https://musicbox.amgradio.ru/musicbox" }] },
+    { name: "CHILLA FM", logoUrl: "img/radio/chilla_fm.png", streams: [
+        { name: "Основной поток", url: "https://chilla.amgradio.ru/ChillaFM" }] },
+    { name: "LITE FM", logoUrl: "img/radio/lite_fm.png", streams: [
+        { name: "Основной поток", url: "https://litefm.amgradio.ru/litefm" }] },
+    { name: "ДЕТСКИЙ ХИТ", logoUrl: "img/radio/detskij_hit.png", streams: [
+        { name: "Основной поток", url: "https://deti.amgradio.ru/Deti" }] },
+    { name: "СКАЗКА FM", logoUrl: "img/radio/skazka_fm.png", streams: [
+        { name: "Основной поток", url: "https://skazka.amgradio.ru/Skazka" }] },
+    { name: "УМНОЕ РАДИО", logoUrl: "img/radio/umnoe_radio.png", streams: [
+        { name: "Основной поток", url: "https://umnoe.amgradio.ru/Umnoe" }] },
+    { name: "LATINO RADIO", logoUrl: "img/radio/latino_radio.png", streams: [
+        { name: "Основной поток", url: "https://latino.amgradio.ru/latino" }] },
+    { name: "РАДИО МЕЧТА", logoUrl: "img/radio/radio_mechta.png", streams: [
+        { name: "Основной поток", url: "https://mechta.amgradio.ru/mechta" }] },
+    { name: "RADIO FUNK", logoUrl: "img/radio/radio_funk.png", streams: [
+        { name: "Основной поток", url: "https://funk.amgradio.ru/Funk" }] },
+    { name: "RBS FM", logoUrl: "img/radio/rbs_fm.png", streams: [
+        { name: "Основной поток", url: "https://rbs.amgradio.ru/rbs" }] },
+    { name: "MAKKIRUS", logoUrl: "img/radio/makkirus.png", streams: [
+        { name: "Основной поток", url: "https://makkirus.amgradio.ru/makkirus" }] },
+    { name: "CLASSIC FM", logoUrl: "img/radio/classic_fm.png", streams: [
+        { name: "Основной поток", url: "https://classic.amgradio.ru/ClassicFM" }] },
+    { name: "РАДИО ЗВУКИ ПРИРОДЫ", logoUrl: "img/radio/radio_zvuki_prirody.png", streams: [
+        { name: "Основной поток", url: "https://zvuki.amgradio.ru/Zvuki" }] },
+    { name: "Спутник Радио", logoUrl: "img/radio/sputnik_radio.png", streams: [
+        { name: "Основной поток", url: "https://stream.pcradio.ru/sputnik_ria_ru-med" }] },
+    { name: "ТНТ Music Radio", logoUrl: "img/radio/tnt_music_radio.png", streams: [
         { name: "128 kbps", url: "https://tntradio.hostingradio.ru:8027/tntradio128.mp3" }] },
 ];
