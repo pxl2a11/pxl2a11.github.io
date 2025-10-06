338dd1a43439208f8619c2cca88f76e681390484
pxl2a11 <pxl2a11@gmail.com>
2025-10-06 08:35:24 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -37,6 +37,7 @@ const appNameToModuleFile = {
     'Форматировщик JSON': 'jsonFormatter',
     'Пасьянс «Косынка»': 'solitaire',
     'Пасьянс Маджонг': 'mahjongSolitaire',
+    'Табата-таймер': 'tabataTimer',
 };
 const appPopularity = {
     'speedTest': 95, 'radio': 88, 'notesAndTasks': 92, 'qrCodeGenerator': 94, 'passwordGenerator': 85, 'unitConverter': 89, 'myIp': 80, 'soundAndMicTest': 78, 'bmiCalculator': 75, 'timer': 70, 'stopwatch': 68, 
@@ -68,6 +69,7 @@ const appPopularity = {
     'jsonFormatter': 87,
     'solitaire': 91,
     'mahjongSolitaire': 89,
+    'tabataTimer': 86,
 };
 const appSearchMetadata = {
     'speedTest': { keywords: ['интернет', 'скорость', 'speed', 'test', 'пинг', 'ping'], hashtags: ['#internet', '#tools'] }, 
@@ -135,6 +137,7 @@ const appSearchMetadata = {
     'jsonFormatter': { keywords: ['json', 'формат', 'валидатор', 'проверка', 'сжать', 'минимизировать', 'formatter', 'validator', 'minify'], hashtags: ['#tools', '#webdev', '#text'] },
     'solitaire': { keywords: ['игра', 'карты', 'пасьянс', 'косынка', 'солитер', 'klondike'], hashtags: ['#game', '#fun', '#logic'] },
     'mahjongSolitaire': { keywords: ['игра', 'маджонг', 'пасьянс', 'кости', 'головоломка', 'логика', 'mahjong', 'solitaire'], hashtags: ['#game', '#fun', '#logic'] 
+    'tabataTimer': { keywords: ['табата', 'интервальный', 'таймер', 'тренировка', 'фитнес', 'спорт', 'работа', 'отдых'], hashtags: ['#time', '#tools', '#health', '#sport'] },
     },
 };
 const moduleFileToAppName = Object.fromEntries(
